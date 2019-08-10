8759edf24d5a448eb325b08000125aa6c5da445a
Michael Scharl <michael.scharl@massiveart.com>
2019-08-10 23:05:26 +0200

    Reverted to `rollup-plugin-typescript`

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,7 @@
 import banner from 'rollup-plugin-banner';
 import commonjs from 'rollup-plugin-commonjs';
 import resolve from 'rollup-plugin-node-resolve';
-import typescript from 'rollup-plugin-typescript2';
+import typescript from 'rollup-plugin-typescript';
 import pkg from './package.json';
 
 const bannerText = [
